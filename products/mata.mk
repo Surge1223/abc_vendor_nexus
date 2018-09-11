2beae046f92a897e07a7778ec11a2a697f8d4059
Surge Raval <Surge1223@gmail.com>
2018-09-11 03:58:36 +0200

    update mata fingerprint for PPR1.180905.036

--- products/mata.mk
+++ products/mata.mk
@@ -31,8 +31,8 @@ PRODUCT_MODEL := PH-1
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mata \
-    BUILD_FINGERPRINT=essential/mata/mata:9/PPR1.180610.091/249:user/release-keys \
-    PRIVATE_BUILD_DESC="mata-user 9 PPR1.180610.091 249 release-keys"
+    BUILD_FINGERPRINT=essential/mata/mata:9/PPR1.180905.036/296:user/release-keys \
+    PRIVATE_BUILD_DESC="mata-user 9 PPR1.180905.036 296 release-keys"
 
 $(call inherit-product-if-exists, vendor/essential/mata/mata-vendor.mk)
 
